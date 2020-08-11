dd8d054440d0380091489eb8f6b31ef1bbcc281d
ArshiaSoni <68380708+ArshiaSoni@users.noreply.github.com>
2020-08-11 10:04:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload(){
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(130, 200, 20);
+	 ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground); 
+	 dustbin = new Dustbin(650, 640)
+	 
+	 Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(250);
+
+  drawSprites();
+
+  fill ("brown");
+  rect (ground.position.x, ground.position.y, width, 10);
+
+  paper.display();
+  dustbin.display();
+}
+
+function keyPressed() {
+	if (keyCode == UP_ARROW){
+
+		Matter.Body.applyForce(paper.body, paper.body.position ,{x:67,y:-67});
+
+	}
+}
+
+
+
+
